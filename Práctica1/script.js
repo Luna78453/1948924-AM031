4cab3d4abe952164ae73d4aedb48badf15eb2cec
joseenriquez1404 <manuel.enriquezr@uanl.edu.mx>
2024-08-31 10:53:56 -0600

    practica 3: Mensaje de login y registro

--- Práctica1/script.js
+++ Práctica1/script.js
@@ -1,11 +1,16 @@
-function imprimirTexto(){
-    var campoTexto = document.getElementById("campoTXT");
-    var texto = campoTexto.value;
+var usuarioTxt = document.getElementById("usuarioTxt");
+var contraseniaTxt = document.getElementById("contraseniaTxt");
 
-    console.log(texto);
+var parrafo = document.querySelector("p");
+
+function iniciarSesion(){
+    var usuarioEntrada = usuarioTxt.value;
+    //parrafo.innerHTML = "Login correcto " + usuarioEntrada;
+
+    parrafo.innerHTML = `Registro con éxito <strong>${usuarioEntrada}</strong>`
 }
 
-function borrarTexto(){
-    var campoTexto = document.getElementById("campoTXT");
-    campoTexto.value = '';
+function registro(){
+    var usuarioEntrada = usuarioTxt.value;
+    parrafo.innerHTML = "Registro con éxito " + usuarioEntrada;
 }
